95130e1df5a61431912cd357459d82627031c071
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-08-14 19:59:35 +0200

    Add Topics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Home from './Home';
 import About from './About';
 import Users from './Users';
+import Topics from './Topics';
 
 class App extends Component {
   render() {
@@ -20,11 +21,17 @@ class App extends Component {
             <li>
               <Link to="/users/7">Users</Link>
             </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
           </ul>
-
-          <Route exact path="/" component={Home} />
-          <Route path="/about/:id" component={About} />
-          <Route path="/users/:id" component={Users} />
+          <hr />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about/:id" component={About} />
+            <Route path="/users/:id" component={Users} />
+            <Route path="/topics" component={Topics} />
+          </Switch>
         </div>
       </Router>
     );
